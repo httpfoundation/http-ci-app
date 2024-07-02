653a83191da3417067bb90ed30e83e025f14e3d4
Zoltan Sisak <zoltan.sisak@gmail.com>
2024-07-02 14:18:15 +0200

    update3

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import roomsRouter from "./routes/rooms";
 import bookingRouter from "./routes/bookings";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
